6587be73c92b52a7bfa3db2cbc929f3467f4b5d5
Officialboss1 <mikeedaboss1@gmail.com>
2022-10-21 05:02:36 -0700

    C - Debugging

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#iclude <stdio.h>
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void pint_remaning_days(int month, int day, int years);
+int convert_dats(int month, int day);
+
+#endif
